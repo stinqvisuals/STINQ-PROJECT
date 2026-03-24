54756307070f565a08afc3c158763c624d0a4b9d
unknown <damasuswastika@gmail.com>
2026-03-24 20:53:03 +0700

    UPDATE 24/03/2026 AT 20.52 PM

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Tambahkan pengecekan ini agar Vercel tidak bingung saat build
+if (!supabaseUrl || !supabaseAnonKey) {
+    console.error("Missing Supabase Env Variables")
+}
+
+export const supabase = createClient(
+    supabaseUrl || '',
+    supabaseAnonKey || ''
+)
