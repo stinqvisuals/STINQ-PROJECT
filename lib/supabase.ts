53ec2b55ca1f97c162d6375f0bcb60ef34d1c006
unknown <damasuswastika@gmail.com>
2026-03-28 15:06:10 +0700

    UPDATE 28/03/2026 AT 15.06 PM

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Ambil variabel dari environment
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-// JIKA variabel kosong (saat build), gunakan URL placeholder agar library tidak error
-// JIKA variabel ada (saat runtime), gunakan nilai aslinya
 export const supabase = createClient(
-    supabaseUrl || 'https://oeqsfqfypwmqzhexkozi.supabase.co', // Ganti dengan URL Supabase kamu
-    supabaseAnonKey || 'dummy-key'
+    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://oeqsfqfypwmqzhexkozi.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lcXNmcWZ5cHdtcXpoZXhrb3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2ODY3MjQsImV4cCI6MjA4OTI2MjcyNH0.zPShh5ObeZxDrC4e56gDtRcv_4K-nhETGfPpc5_bkis'
 )
