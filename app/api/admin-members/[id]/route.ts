84b8da1d73cd61f48fb3b7e396805c578c385928
unknown <damasuswastika@gmail.com>
2026-03-28 16:26:48 +0700

    UPDATE 28/03/2026 AT 16.26 PM

--- app/api/admin-members/[id]/route.ts
+++ app/api/admin-members/[id]/route.ts
@@ -8,7 +8,7 @@ export async function DELETE(req: Request) {
     // 🔥 INISIALISASI DI DALAM FUNGSI (Solusi Final Error Vercel)
     // Dengan cara ini, library tidak akan protes "URL is required" saat proses build
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://oeqsfqfypwmqzhexkozi.supabase.co';
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy-key-untuk-build';
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lcXNmcWZ5cHdtcXpoZXhrb3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2ODY3MjQsImV4cCI6MjA4OTI2MjcyNH0.zPShh5ObeZxDrC4e56gDtRcv_4K-nhETGfPpc5_bkis';
 
     const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
